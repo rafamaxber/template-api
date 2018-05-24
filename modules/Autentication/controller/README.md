f4a9321508bb740d3694d3f9132980a7433d80c6
Rafael Bernardo <rafamaxber@gmail.com>
2018-05-24 01:37:05 -0300

    start domain drive development

---
+++ modules/Autentication/controller/README.md
@@ -0,0 +1,3 @@
+<!--
+  autentication controller
+-->
---
+++ modules/Autentication/index.js
@@ -0,0 +1,15 @@
+// autentication config
+
+/* TODO:
+* /cadastro
+* /me
+* /login
+* /recupera senha
+*/
+
+new Autentication({
+  fbid: '',
+  googleId: '',
+  loginSchema,
+  CadasterSchema,
+});
