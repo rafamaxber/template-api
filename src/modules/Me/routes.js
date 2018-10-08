e59224a7424c33c4067ceffc81f5262c11ca19d3
Rafael Bernardo <rafamaxber@gmail.com>
2018-10-08 01:30:31 -0300

    Add docker-compose with mongo images to develop

--- src/modules/Me/routes.js
+++ src/modules/Me/routes.js
@@ -3,7 +3,8 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.status(200).json({ name: 'Rafael' })
+  console.log(process.env)
+  res.status(200).json({ name: 'Rafael', lastname: 'Talking' })
 })
 
 export default router
